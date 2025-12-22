0e5c7446bf059bb0f2bdabf4d933e3fb57b84720
Julian Marius Heinze <julian.h12@protonmail.com>
2025-12-22 14:06:55 +0100

    volle eval vom cnn per-concept/per-vector eval in diesem punkt lowkey fertig

--- src/Trainer/CBMTrainer.py
+++ src/Trainer/CBMTrainer.py
@@ -15,7 +15,7 @@ from Models.Model2 import Model
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 class CBMTrainer:
-    def __init__(self, device, model, train_loader, val_loader):
+    def __init__(self, device, model, train_loader, val_loader, patience = 5):
         self.device = device
         self.model = model
         
@@ -29,6 +29,19 @@ class CBMTrainer:
             optim.Adam(self.model.cnn.parameters(), lr=0.0005),
             optim.Adam(self.model.concept.parameters(), lr=0.0005)
         ]
+
+        #early stopping and history metrics
+
+        self.patience = patience
+
+        self.best_value_loss = float('inf')
+        self.current_patience = 0
+
+        self.history = {
+            "train_loss" : [],
+            "per-concept train_acc" : [],
+            "val_loss" : [],
+            "per-concept val_acc" : []}
 
 
     def freeze(self, model):
@@ -58,7 +71,7 @@ class CBMTrainer:
 
             self.training(0)
             self.validation(0)
-
+            ##IMPORTANT EARLY STOPPING FUNCTION
             # Calculate epoch statistics TODO
 
         self.unfreeze(self.model.concept)
@@ -83,10 +96,7 @@ class CBMTrainer:
         self.model.train()
 
         total_loss = 0.0
-        total_samples = 0
-        correct_predictions = 0
-        concept_correct = 0
-        concept_total = 0
+        total_samples, vector_correct, concept_correct, concept_total, class_correct = 0,0,0,0,0
 
         for batch_id, (data, (vector, label)) in enumerate(self.train_loader):
             data = data.to(self.device)
@@ -103,45 +113,44 @@ class CBMTrainer:
             loss.backward()
             self.optimizer[phase].step()
 
-            #accuracy calculation
             total_loss += loss.item()
 
             if (phase == 0):
                 predicted = (torch.sigmoid(output) > 0.5).float()
 
                 #per vector accuracy
+                #a vector only counts as correct if all items in it are correct (strict implementation of correctness)
                 batch_size = target.size(0)
-                vector_pred_correct = (predicted == target).all(dim=1).sum().item()
-                correct_predictions += vector_pred_correct
                 total_samples += batch_size
+                vector_correct += (predicted == target).all(dim=1).sum().item()
                 
                 #per concept accuracy
                 concept_correct += (predicted == target).sum().item()
                 concept_total += target.numel()
+
             else:
                 predicted = torch.argmax(output, dim=1)
                 total_samples += target.size(0)
-            
-            correct_predictions += (predicted == target).sum().item()
+                class_correct += (predicted == target).sum().item()
 
             # Print progress
             if batch_id % 100 == 0:
                 current_loss = total_loss / (batch_id + 1)
                 if phase == 0:
-                    vector_acc = 100. * correct_predictions / total_samples if total_samples > 0 else 0
+                    vector_acc = 100. * vector_correct / total_samples if total_samples > 0 else 0
                     concept_acc = 100. * concept_correct / concept_total if concept_total > 0 else 0
-                    print(f"Training Batch {batch_id:3d}: Loss = {current_loss:.4f} | Vector Acc: {vector_acc:.4f}% | Per-Concept Acc: {concept_acc:.4f}")
+                    print(f"Training Batch {batch_id:3d}: Loss = {current_loss:.4f} | Vector Acc: {vector_acc:.4f}% | Per-Concept Acc: {concept_acc:.4f}%")
                 else:
-                    train_acc = 100. * correct_predictions / total_samples
+                    train_acc = 100. * class_correct / total_samples
                     print(f"Training Batch {batch_id:3d}: Loss = {current_loss:.4f} | Acc: {train_acc:.4f}%")
         return
     
     def validation(self, phase):
         print("VALIDATE")
         self.model.eval()
+
         total_loss = 0.0
-        correct_predictions = 0
-        total_samples = 0
+        total_samples, vector_correct, concept_correct, concept_total, class_correct = 0,0,0,0,0
 
         with torch.no_grad():
             for batch_id, (data, (vector, label)) in enumerate(self.val_loader):
@@ -157,21 +166,34 @@ class CBMTrainer:
 
                 if (phase == 0):
                     predicted = (torch.sigmoid(output) > 0.5).float()
-                    total_samples += target.numel()
+
+                    #per vector accuracy
+                    #a vector only counts as correct if all items in it are correct (strict implementation of correctness)
+                    batch_size = target.size(0)
+                    total_samples += batch_size
+                    vector_correct += (predicted == target).all(dim=1).sum().item()
+                
+                    #per concept accuracy
+                    concept_correct += (predicted == target).sum().item()
+                    concept_total += target.numel()
+
                 else:
                     predicted = torch.argmax(output, dim=1)
                     total_samples += target.size(0)
-                
-                correct_predictions += (predicted == target).sum().item()
-               
+                    class_correct += (predicted == target).sum().item()
+
+                average_loss = total_loss / len(self.val_loader)
+
+                # Print progress
                 if batch_id % 100 == 0:
                     current_loss = total_loss / (batch_id + 1)
-                    train_acc = 100. * correct_predictions / total_samples
-                    print(f"Training Batch {batch_id:3d}: Loss = {current_loss:.4f} | Acc: {train_acc:.2f}%")
-        return
-
-    def caculate_concept_metrics():
-
+                    if phase == 0:
+                        vector_acc = 100. * vector_correct / total_samples if total_samples > 0 else 0
+                        concept_acc = 100. * concept_correct / concept_total if concept_total > 0 else 0
+                        print(f"Validation Batch {batch_id:3d}: Loss = {current_loss:.4f} | Average Loss = {average_loss:.4f} | Vector Acc: {vector_acc:.4f}% | Per-Concept Acc: {concept_acc:.4f}%")
+                    else:
+                        train_acc = 100. * class_correct / total_samples
+                        print(f"Validation Batch {batch_id:3d}: Loss = {current_loss:.4f} | Acc: {train_acc:.4f}%")
         return
     
 if __name__ == "__main__":
